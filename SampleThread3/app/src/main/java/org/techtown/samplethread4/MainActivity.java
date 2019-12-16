f88642e98c66eea39011b724fd91e1d838f9d6b5
kangmin1012 <kangmin1012@naver.com>
2019-12-16 12:10:51 +0900

    Handler를 이용한 스레드사용

---
+++ SampleThread3/app/src/main/java/org/techtown/samplethread4/MainActivity.java
@@ -0,0 +1,61 @@
+package org.techtown.samplethread4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackgroundThread thread = new BackgroundThread();
+                thread.start();
+            }
+        });
+
+
+    }
+
+    class BackgroundThread extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i = 0 ; i <100 ; i++) {
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){}
+
+                value += 1;
+                Log.d("Thread","value : " + value);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("value 값: " + value);
+                    }
+                });
+
+            }
+        }
+    }
+
+
+}
