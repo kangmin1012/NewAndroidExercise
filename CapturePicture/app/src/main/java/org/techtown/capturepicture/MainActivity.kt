102bf43a6cc693cc371cf75bfa282ed2338fe741
kangmin1012 <kangmin1012@naver.com>
2020-02-19 16:17:53 +0900

    Capturing Picture Original Size

--- CapturePicture/app/src/main/java/org/techtown/capturepicture/MainActivity.kt
+++ CapturePicture/app/src/main/java/org/techtown/capturepicture/MainActivity.kt
@@ -4,22 +4,28 @@ package org.techtown.capturepicture
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import com.gun0912.tedpermission.PermissionListener
+import java.io.IOException
 import java.security.Permission
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
-
+    lateinit var currentPhotoPath : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,16 +68,45 @@ class MainActivity : AppCompatActivity() {
     fun startCapture(){
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                val photoFile: File? = try{
+                    createImageFile()
+                }catch(ex:IOException){
+                    null
+                }
+                photoFile?.also{
+                    val photoURI : Uri = FileProvider.getUriForFile(
+                        this,
+                        "org.techtown.capturepicture.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
             }
         }
     }
 
+    @Throws(IOException::class)
+    private fun createImageFile() : File{
+        val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply{
+            currentPhotoPath = absolutePath
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            img_picture.setImageBitmap(imageBitmap)
+           val file = File(currentPhotoPath)
+            val bitmap = MediaStore.Images.Media
+                .getBitmap(contentResolver, Uri.fromFile(file))
+            img_picture.setImageBitmap(bitmap)
         }
     }
 }
